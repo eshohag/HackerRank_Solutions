0d812b7d0af6d9d82aa9cd5d3f41c08f92a42a60
eshohag <eshohag@outlook.com>
2022-10-22 14:20:37 +0600

    Updates

--- HackerRank_Solutions/Solutions.cs
+++ HackerRank_Solutions/Solutions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.Intrinsics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -256,6 +257,27 @@ namespace HackerRank_Solutions
                     numberMap.Add(a[i], i);
             }
             return minimumDist;
+        }
+        public void findZigZagSequence(List<int> a, int n)
+        {
+            a = a.OrderBy(a => a).ToList();
+            int mid = (n - 1) / 2;
+            swap(a[mid], a[n - 1]);
+
+            int st = mid + 1;
+            int ed = n - 2;
+            while (st <= ed)
+            {
+                swap(a[st], a[ed]);
+                st = st + 1;
+                ed = ed - 1;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (i > 0) cout << " ";
+                cout << a[i];
+            }
+            cout << endl;
         }
     }
 }
